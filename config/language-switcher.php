77a2b5d72e0ea1513e0593af73140f124e22a9ba
Abdiwaahid <abdiwaahid01@gmail.com>
2025-08-22 12:00:33 +0200

    feat: session driver

--- config/language-switcher.php
+++ config/language-switcher.php
@@ -2,8 +2,15 @@
 
 // config for Abdiwaahid/LanguageSwitcher
 return [
+
+    // 'session' or 'cache'
+    'driver' => 'session',
+
+    // The guard to use for authenticated users.
+    // If null, will use the default guard.
     'guard' => null,
 
+    // The key used in the storage driver.
     'key' => 'locale_',
 
     'languages' => [
--- src/LanguageSwitcher.php
+++ src/LanguageSwitcher.php
@@ -15,31 +15,43 @@ class LanguageSwitcher
 
     public static function set($locale)
     {
-        $key = static::getKey();
-        Cache::forever($key, $locale);
-
+        $driver = config('language-switcher.driver', 'session');
+        if ($driver === 'session') {
+            return session()->put(static::getKey(), $locale);
+        }else{
+            Cache::forever(static::getKey(), $locale);
+        }
         return $locale;
     }
 
     public static function get()
     {
+
+        $driver = config('language-switcher.driver', 'session');
+        if ($driver === 'session') {
+            return session()->get(static::getKey(), app()->getLocale());
+        }
         return Cache::get(static::getKey(), app()->getLocale());
     }
 
     public static function getKey(): string
     {
-        $guard = config('language-switcher.guard');
-        $key = config('language-switcher.key');
-        if ($guard) {
-            return Auth::check() ? $key.auth($guard)->id() : $key.request()->ip();
+        $key = config('language-switcher.key', 'locale_');
+        if (config('language-switcher.driver') === 'session') {
+            return $key;
         }
 
-        return $key.request()->ip();
+        $guard = config('language-switcher.guard');
+        if ($guard && Auth::guard($guard)->check()) {
+            return $key . Auth::guard($guard)->id();
+        }
+
+        return $key . request()->ip();
     }
 
     public static function languages()
     {
-        return collect(config('language-switcher.languages'))->filter(fn ($name, $locale) => $locale !== static::get());
+        return collect(config('language-switcher.languages'))->filter(fn($name, $locale) => $locale !== static::get());
     }
 
     public static function translationKeyFallback($key, $params, $fallback)
