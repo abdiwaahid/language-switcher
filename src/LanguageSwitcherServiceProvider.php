57973673598f830d7b6831da59400f4a8fb7ed75
Abdiwaahid <abdiwaahid01@gmail.com>
2025-08-27 14:54:07 +0200

    asset publishing and directives

--- src/LanguageSwitcherServiceProvider.php
+++ src/LanguageSwitcherServiceProvider.php
@@ -5,6 +5,7 @@ namespace Abdiwaahid\LanguageSwitcher;
 use Abdiwaahid\LanguageSwitcher\Commands\LanguageSwitcherCommand;
 use Abdiwaahid\LanguageSwitcher\Http\Middleware\LanguageSwitcherMiddleware;
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Blade;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -28,6 +29,22 @@ class LanguageSwitcherServiceProvider extends PackageServiceProvider
 
     public function packageBooted()
     {
+        $this->publishes([
+            __DIR__ . '/../resources/assets/css/main.css' => public_path('css/language-switcher/main.css'),
+            __DIR__ . '/../resources/assets/js/index.js' => public_path('js/language-switcher/index.js'),
+        ], 'language-switcher-assets');
+
+        Blade::directive('languageSwitcherStyles', function () {
+            return <<<HTML
+                <link rel="stylesheet" href="{{ asset('css/language-switcher/main.css') }}">
+            HTML;
+        });
+        Blade::directive('languageSwitcherScripts', function () {
+            return <<<HTML
+                <script src="{{ asset('js/language-switcher/index.js') }}"></script>
+            HTML;
+        });
+        
         $router = $this->app->make(Router::class);
         $router->pushMiddlewareToGroup('web', LanguageSwitcherMiddleware::class);
     }
